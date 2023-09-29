3070ff3a9a9d7f0e468f423ecd7ace64801f94e4
Kevin <kevinmathew2002@gmail.com>
2023-09-29 22:52:41 +0100

    Delete booking loading spinner

--- frontend/src/components/UserPanel.vue
+++ frontend/src/components/UserPanel.vue
@@ -2,7 +2,7 @@
 import {useUserStore} from "@/stores/user";
 import {useMutation, useQuery} from "@vue/apollo-composable";
 import {ALL_BOOKINGS, DELETE_BOOKING} from "@/queries";
-import {computed} from "vue";
+import {computed, ref, watch} from "vue";
 import type {Booking} from "@/types";
 import {Temporal} from "temporal-polyfill";
 import {getMonday} from "@/utils/datetime";
@@ -10,9 +10,11 @@ import {getMonday} from "@/utils/datetime";
 
 const userStore = useUserStore()
 
-const {result, loading} = useQuery(ALL_BOOKINGS)
-const userBookings = computed<Booking[]>(() => result.value?.allBookings?.filter((obj: Booking)=>obj.user.id === userStore.user?.id) ?? [])
+const {result} = useQuery(ALL_BOOKINGS)
+const userBookings = computed<Booking[]>(() => result.value?.allBookings?.filter((obj: Booking) => obj.user.id === userStore.user?.id) ?? [])
 
+const deleteTarget = ref<string | number | null>(null)
+const loading = ref(false)
 
 interface readQueryType {
   allBookings: any[]
@@ -33,12 +35,18 @@ const {mutate: deleteBookingMutation} = useMutation(DELETE_BOOKING, () => ({
 }))
 
 function deleteBooking(id: number | string): void {
+  loading.value = true
+  deleteTarget.value = id
   deleteBookingMutation({
     bookingID: id
   })
 
 }
 
+watch(userBookings, () => {
+  deleteTarget.value = null
+  loading.value = false
+})
 
 </script>
 
@@ -46,7 +54,8 @@ function deleteBooking(id: number | string): void {
   <div v-if="userStore.user" class="container-fluid card mt-4 bg-white p-3 rounded" style="max-width: 1200px">
     <div class="d-flex mb-4">
       <span class="fs-3 me-2">Hello, {{ userStore.user?.firstName }}</span>
-      <span class="badge bg-primary mx-1 fs-6 align-self-center font-monospace" v-for="group of userStore.groups">{{ group }}</span>
+      <span class="badge bg-primary mx-1 fs-6 align-self-center font-monospace"
+            v-for="group of userStore.groups">{{ group }}</span>
     </div>
 
 
@@ -56,7 +65,12 @@ function deleteBooking(id: number | string): void {
         <div class="p-2 card bg-light" style="line-height: 0.5em; min-width: 160px;" v-for="booking in userBookings">
           <div>
             <span class="badge bg-danger fs-6">{{ booking.court.name }}</span>
-            <button class="btn-close float-end" @click="deleteBooking(booking.id)"></button>
+            <div v-if="loading && deleteTarget === booking.id" class="align-self-center float-end m-1">
+              <div class="spinner-border" role="status" style="width: 20px; height: 20px;">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+            <button v-else class="btn-close float-end" @click="deleteBooking(booking.id)"></button>
           </div>
           <p class="mt-3 fw-bold">{{
               Temporal.PlainDate.from(booking.date).toLocaleString('en-GB', {
@@ -71,7 +85,8 @@ function deleteBooking(id: number | string): void {
       </div>
     </div>
 
-    <div v-else class="fs-5"><p class="mb-0">You have no upcoming bookings.</p><p>Use the calendar below to make one:</p>   </div>
+    <div v-else class="fs-5"><p class="mb-0">You have no upcoming bookings.</p>
+      <p>Use the calendar below to make one:</p></div>
   </div>
 </template>
 
